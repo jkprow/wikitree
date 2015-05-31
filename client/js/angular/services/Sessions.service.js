8889f52fef8811b1ec583cdecc99e40cfa8fc65d
jkprow <jkprow@gmail.com>
2015-05-31 14:35:25 -0700

    welcome page with mystery no-node error

--- client/js/angular/services/Sessions.service.js
+++ client/js/angular/services/Sessions.service.js
@@ -161,7 +161,7 @@
 
                     // if deleted only session:
                     if (Sessions.index.length == 0) {
-                        window.location = '/';
+                        window.location = '/welcome';
                     // if deleted active session that was last:
                     } else if (idx == Sessions.active) {
                         var uuid;
