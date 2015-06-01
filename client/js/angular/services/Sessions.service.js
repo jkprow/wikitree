02949a5cc8fffcb264dd2a23ad4de46ed58e4922
jkprow <jkprow@gmail.com>
2015-06-01 05:40:10 -0700

    window.location ftw

--- client/js/angular/services/Sessions.service.js
+++ client/js/angular/services/Sessions.service.js
@@ -89,7 +89,9 @@
                     localStorageService.set('index', Sessions.index);
                     localStorageService.set('active', Sessions.active);
 
-                    $location.path('/'+session.uuid);
+                    console.log('new session', session.uuid);
+
+                    $location.path('/session/'+session.uuid);
                     return session;
                 };
 
@@ -138,6 +140,8 @@
 
                 Sessions.restore = function (uuid) {
                     var session = localStorageService.get(uuid);
+
+                    console.log('restored session', session);
 
                     if (!session) $location.path('/');
 
